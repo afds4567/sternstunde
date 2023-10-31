a399e2a35c6684fb0e017508a5ce16faf7542cf9
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-10-31 13:12:31 +0900

    feat: implement mobile navBar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
-import { IoCloseOutline } from "react-icons/io5";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen((prev) => !prev);
   return (
     <>
       <div className="navBar">
@@ -26,15 +27,33 @@ export default function Navbar() {
           className="navBar__button__mobile"
           onClick={() => setIsOpen((prev) => !prev)}
         >
-          {isOpen ? <IoCloseOutline /> : <AiOutlineMenu />}
+          {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
         </div>
       </div>
       {isOpen && (
         <div className="navBar__mobile">
           <div className="navBar__menu__mobile">
-            <Link href="/places">List</Link>
-            <Link href="/places/new">Register</Link>
-            <Link href="/users/login">Login</Link>
+            <Link
+              className="navBar__item__mobile"
+              href="/places"
+              onClick={toggleOpen}
+            >
+              List
+            </Link>
+            <Link
+              className="navBar__item__mobile"
+              href="/places/new"
+              onClick={toggleOpen}
+            >
+              Register
+            </Link>
+            <Link
+              className="navBar__item__mobile"
+              href="/users/login"
+              onClick={toggleOpen}
+            >
+              Login
+            </Link>
           </div>
         </div>
       )}
